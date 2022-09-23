0e4ccae94b5b1f5ca499c1ae85bd11ecbcbbeb66
gasmartin12 <gaston.gmartin@mercadolibre.com>
2022-09-23 12:38:03 -0300

    exercise 1 class 1

---
+++ goBasic/ejer1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Gaston Martin"
+	address := "Cabrera 642 - Rio Cuarto - Cordoba - Arg"
+	fmt.Println("My name is: ", name)
+	fmt.Println("My address is: ", address)
+}
