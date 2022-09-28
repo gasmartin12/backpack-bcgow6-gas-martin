d9721bd942a659f5784c9abaa9049753bc6ccfcf
gasmartin12 <gaston.gmartin@mercadolibre.com>
2022-09-28 15:50:44 -0300

    class 7, exer 2

---
+++ goBasic/classSeven/exer2.go
@@ -0,0 +1,37 @@
+/*
+Ejercicio 2 - Impuestos de salario #2
+
+Haz lo mismo que en el ejercicio anterior pero reformulando
+el código para que, en reemplazo de "Error()",
+se implemente "errors.New()".
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var salaryMin = 150000
+var msg = "You must pay tax"
+var errorMsg = "the salary paid does not the taxable minimum"
+
+func getSalaryError(salary int) (code int, err error) {
+	if salary < salaryMin {
+		return 500, errors.New(errorMsg)
+	}
+	return 200, nil
+}
+
+func main() {
+	salary := 20000
+	code, err := getSalaryError(salary)
+	if code != 200 {
+		fmt.Printf("Error: %d\n %s\n", code, err)
+		os.Exit(1)
+	}
+	fmt.Println(msg)
+
+}
