3a4d7f63c6425777327d7d319644ca9009753e49
gasmartin12 <gaston.gmartin@mercadolibre.com>
2022-09-28 15:50:39 -0300

    class 7, exer 1

---
+++ goBasic/classSeven/exer1.go
@@ -0,0 +1,52 @@
+/*
+Ejercicio 1 - Impuestos de salario #1
+En tu función "main", define una variable llamada "salary"
+y asignarle un valor de tipo "int".
+Crea un error personalizado con un struct que implemente
+"Error()" con el mensaje "error: el salario ingresado no
+alcanza el mínimo imponible" y lánzalo en caso de que "salary"
+sea menor a 150.000. Caso contrario, imprime por consola el
+mensaje "Debe pagar impuesto".
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var salaryMin = 150000
+var msg = "You must pay tax"
+var errorMsg = "the salary paid does not the taxable minimum"
+
+type SalaryError struct {
+	StatusCode int
+	Message    string
+}
+
+func (err *SalaryError) Error() string {
+	return fmt.Sprintf("Error: %d\n %s", err.StatusCode, err.Message)
+}
+
+func getSalaryError(salary int) (code int, err error) {
+	if salary < salaryMin {
+		return 500,
+			&SalaryError{
+				StatusCode: 500,
+				Message:    errorMsg,
+			}
+	}
+	return 200, nil
+}
+
+func main() {
+	salary := 20000
+	code, err := getSalaryError(salary)
+	if code != 200 {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(msg)
+
+}
